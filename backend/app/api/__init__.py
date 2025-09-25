47396d851457da55a5b6fc06f298fec47f5e812b
ohad12345678 <ohadbanay4@gmail.com>
2025-09-25 18:51:58 +0300

    Delete backend directory

--- backend/app/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .v1 import v1_router
-
-__all__ = ["v1_router"]
--- backend/app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from .auth_service import AuthService
-from .claude_service import ClaudeService  
-from .check_service import CheckService
-
-__all__ = [
-    "AuthService",
-    "ClaudeService", 
-    "CheckService"
-]
